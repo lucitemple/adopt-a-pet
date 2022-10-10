06d5484f3c9af66b2fad06d5026f2a0aa41a3167
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-10-10 20:01:05 +1100

    codecademy react-router-dom tutorial

---
+++ src/components/navigation/index.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getPetTypes } from '../../api/petfinder';
+import Logo from '../../assets/logo.svg';
+import Search from '../search';
+
+const Navigation = () => {
+  const [petTypes, setPetTypes] = useState([]);
+
+  useEffect(() => {
+    async function getPetTypesData() {
+      const { types } = await getPetTypes();
+      setPetTypes(types);
+    }
+
+    getPetTypesData();
+  }, []);
+
+  return (
+    <nav>
+      <div className="nav-logo">
+        <img src={Logo} alt="Petlover" />
+        <Search />
+      </div>
+      <ul className="nav-links">
+        <li key="all">
+          <NavLink
+            exact
+            to="/"
+            className="nav-link"
+            activeClassName="nav-link-active"
+          >
+            All Pets
+          </NavLink>
+        </li>
+        {petTypes
+          ? petTypes.map((type) => (
+              <li key={type.name}>
+                <NavLink
+                  // Below line is meant to work but is causing a failed build
+                  // to={`/${type._links.self.to.split('/').pop()}`}
+                  to={`/${type.name}`} // temp workaround
+                  key={type.name}
+                  className="nav-link"
+                  activeClassName="nav-link-active"
+                >
+                  {type.name}s
+                </NavLink>{' '}
+              </li>
+            ))
+          : 'Loading...'}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
